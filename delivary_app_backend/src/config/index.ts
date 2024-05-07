f7f552fb1fea87c749203cc93cb2ea4cd10c9621
Nero15275 <91941603+Nero15275@users.noreply.github.com>
2024-05-07 09:24:32 +0530

    Delete delivary_app_backend directory

--- delivary_app_backend/src/config/index.ts
+++
@@ -1,3 +0,0 @@
-// export const dbUrl="mongodb+srv://nero:123456789s@cluster0.vpgikjs.mongodb.net/dilevery_app"
-export const dbUrl="mongodb://localhost:27017/dilevery_app"
-export const jwt_Key ="dawdawdaffbfb3423@#@$"
